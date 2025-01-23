60b8f9255eab053a3b27c6419cc2bcd0218c54a6
Natalya Selyunina <seluninanv@mail.ru>
2025-01-24 00:50:03 +0300

    feat Dockerfile, docker-compose.yml, .env

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -1,8 +1,7 @@
 --liquibase formatted sql
 
 --changeset selyunina:1
-CREATE TABLE IF NOT EXISTS postgres.wallet
-(
+CREATE TABLE IF NOT EXISTS wallet.wallet (
     id uuid PRIMARY KEY ,
     balance NUMERIC(19,2) NOT NULL ,
     created_at TIMESTAMP NOT NULL
